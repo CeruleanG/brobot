b0a4d9969adabd9618a47540ee6ae2343bc5172c
CeruleanG <84096851+CeruleanG@users.noreply.github.com>
2026-02-07 15:53:37 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+from keep_alive import keep_alive
+
+# load in the .env file
+load_dotenv()
+# prepare a Flask request to keep the bot alive
+keep_alive()
+
+# Discord bot setup
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+#@bot.command()
+#async def test(ctx):
+#    await ctx.send(f"Hello")
+
+@bot.command()
+async def joue(ctx):
+    await ctx.send(f"{ctx.author.mention} veut jouer, des gens?")
+
+
+# Run the bot
+bot.run(os.getenv('DISCORD_TOKEN'))
